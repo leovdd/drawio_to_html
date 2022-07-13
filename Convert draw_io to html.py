7d9753435dd55618d8143785a3cc5f27704f69dd
leovdd <leovdd@hotmail.com>
2022-07-13 10:40:09 +0200

    Argument parser is ok

--- Convert draw_io to html.py
+++ Convert draw_io to html.py
@@ -1,4 +1,6 @@
-from unittest import result
+import argparse
+import sys
+import os
 from lxml import etree
 
 XSLT_CONTENT = '''<?xml version='1.0'?>
@@ -15,6 +17,40 @@ XSLT_CONTENT = '''<?xml version='1.0'?>
     </xsl:template>
     </xsl:stylesheet>
 '''
+
+parser = argparse.ArgumentParser(description="Removes sheets starting with underscore from draw.io files. Works on both compressed as well as non-compressed files.")
+parser.add_argument ("input", default = 'input.xml', help = "The original file. Defaults to 'input.xml'.")
+parser.add_argument ("-o",help = "The cleaned file. By default it's [input filename]_cleaned.xml")
+parser.add_argument ("--xslt", action=argparse.BooleanOptionalAction, help="Saves the xslt used as 'Remove_underscore_sheets_from_draw.io_file.xslt'. Input filename is mandatory when used !")
+
+input = ""
+output = ""
+
+args = parser.parse_args()
+
+input = args.input   
+
+input_filename, input_extention = os.path.splitext(args.input)
+if args.o:
+    output_filename, output_extention = os.path.splitext(args.o)
+    if output_extention:
+        output = args.o
+    else:
+        output = output_filename + '.xml'
+else:
+    output = input_filename + "_cleaned"
+    if input_extention:
+        output = output + input_extention
+    else:
+        output = output + ".xml"
+
+if args.xslt:
+    with open ('Remove_underscore_sheets_from_draw.io_file.xslt', 'w') as x:
+        x.write(XSLT_CONTENT)
+
+print ("input:" , args.input)
+print ("output:", output)
+        
 
 xslt_doc = etree.XML(XSLT_CONTENT)
 
